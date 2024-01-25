3c8ea2fb396c93619c4feccf3739af2acb89e3d1
OriginalAnswer <akiseonair@gmail.com>
2024-01-25 16:16:21 +0900

    .

--- header.js
+++ header.js
@@ -3,16 +3,22 @@ window.addEventListener('scroll', () => {
     // const h = document.querySelector('header');
     const homelogo = document.querySelector('.homelogo');
     const menuBtn = document.querySelector('.menu-btn');
+    const homelogo2 = document.querySelector('.homelogo2');
+    const menuBtn2 = document.querySelector('.menu-btn2');
     let y = window.scrollY;//현재 스크롤 위치
     if (y > targetY + 150) {
         // h.classList.remove('dpnone')
         // h.style.opacity = '1';
         homelogo.style.fontSize = '1.5em';
         menuBtn.style.fontSize = '1.5em';
+        homelogo2.style.fontSize = '1.5em';
+        menuBtn2.style.fontSize = '1.5em';
     } 
     else {
         // h.classList.add('dpnone')
         homelogo.style.fontSize = '';
         menuBtn.style.fontSize = '';
+        homelogo2.style.fontSize = '';
+        menuBtn2.style.fontSize = '';
     }
-});
+})
